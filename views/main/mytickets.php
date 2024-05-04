e188eedf840f49c5ec056de6c701928ac68a4556
NikolayBondarchik <73456147+NikolayBondarchik@users.noreply.github.com>
2024-05-04 23:11:28 +0600

    a040524

--- views/main/mytickets.php
+++ views/main/mytickets.php
@@ -4,56 +4,54 @@ use App\Post;
 $tickets = Post::findTicketsById();
 ?>
 
-    <?php foreach ($tickets as $ticket): ?>
+<?php foreach ($tickets as $ticket): ?>
 
-        <article class="ticket">
-            <header class="ticket__wrapper">
-                <div class="ticket__header">
-                    Кино|Маршрут
-                </div>
-            </header>
-            <div class="ticket__divider">
-                <div class="ticket__notch"></div>
-                <div class="ticket__notch ticket__notch--right"></div>
+    <article class="ticket">
+        <header class="ticket__wrapper">
+            <div class="ticket__header">
+                Кино|Маршрут
             </div>
-            <div class="ticket__body">
+        </header>
+        <div class="ticket__divider">
+            <div class="ticket__notch"></div>
+            <div class="ticket__notch ticket__notch--right"></div>
+        </div>
+        <div class="ticket__body">
 
-                <section class="ticket__section">
-                    <h3>Мой билет</h3>
-                    <p>Название фильма: <?= $ticket->name ?></p>
-                    <p>Дата показа: <?= $ticket->show_date ?></p>
-                    <p>Время показа: <?= $ticket->show_time ?></p>
-                    <p>Продолжительность: <?= $ticket->duration ?> мин</p>
-                </section>
+            <section class="ticket__section">
+                <h3>Мой билет</h3>
+                <p>Название фильма: <?= $ticket->name ?></p>
+                <p>Дата показа: <?= $ticket->show_date ?></p>
+                <p>Время показа: <?= $ticket->show_time ?></p>
+                <p>Продолжительность: <?= $ticket->duration ?> мин</p>
+            </section>
 
-                <section class="ticket__section">
-                    <h3>Доп информация</h3>
-                    <p>Зал: <?= $ticket->hall ?></p>
-                    <p>Тип: <?= $ticket->type ?></p>
-                    <p>Код билета: <?= $ticket->code ?></p>
+            <section class="ticket__section">
+                <h3>Доп информация</h3>
+                <p>Зал: <?= $ticket->hall ?></p>
+                <p>Тип: <?= $ticket->type ?></p>
+                <p>Код билета: <?= $ticket->code ?></p>
 
-                </section>
+            </section>
 
-                <section class="ticket__section">
-                    <h3>Покупатель</h3>
-                    <p><?= $_SESSION['full_name'] ?></p>
-                </section>
+            <section class="ticket__section">
+                <h3>Покупатель</h3>
+                <p><?= $_SESSION['full_name'] ?></p>
+            </section>
 
-            </div>
-            <footer class="ticket__footer">
-                <span>Стоимость</span>
-                <?php if($ticket->type == "Детский"): ?>
-                    <span><?= $ticket->price_child ?> тг.</span>
-                <?php else: ?>
-                    <span><?= $ticket->price_adult ?> тг.</span>
-                <?php endif; ?>
-            </footer>
-            <form action="qr" method="GET">
-                <input type="hidden" name="code" value="<?= $ticket->code ?>">
-                <button type="submit">ПОКАЗАТЬ QR КОД</button>
-            </form>
-        </article>
+        </div>
+        <footer class="ticket__footer">
+            <span>Стоимость</span>
+            <?php if ($ticket->type == "Детский"): ?>
+                <span><?= $ticket->price_child ?> тг.</span>
+            <?php else: ?>
+                <span><?= $ticket->price_adult ?> тг.</span>
+            <?php endif; ?>
+        </footer>
+        <form class="qr-form" action="qr" method="GET">
+            <input type="hidden" name="code" value="<?= $ticket->code ?>">
+            <button type="submit">ПОКАЗАТЬ QR КОД </button>
+        </form>
+    </article>
 
-    <?php endforeach; ?>
-
-
+<?php endforeach; ?>
